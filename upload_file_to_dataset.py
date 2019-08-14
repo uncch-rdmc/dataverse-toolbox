b7862f95b4a36692703355a1ce7bcc1729effbd6
Don Sizemore <don.sizemore@gmail.com>
2019-08-14 15:17:24 -0400

    support directory upload

--- upload_file_to_dataset.py
+++ upload_file_to_dataset.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 
 from datetime import datetime
+from os import listdir
+from os.path import isfile, join
 import argparse
 import json
+import os
 import requests
 
 # define arguments. persistentid, apitoken and file are required.
-parser = argparse.ArgumentParser(description='Upload a file to a given dataset.')
+parser = argparse.ArgumentParser(description='Upload a file or directory of files to a given dataset.')
 parser.add_argument('-d', '--dataverse', default='https://dataverse.unc.edu', help='Dataverse URL. Defaults to https://dataverse.unc.edu')
 parser.add_argument('-p', '--persistentid', required=True, help='Persistent ID, in the format doi:authority/shoulder/identifier')
 parser.add_argument('-t', '--apitoken', required=True, help='API token. Required.')
-parser.add_argument('-f', '--file', required=True, help='File to upload.')
+parser.add_argument('-f', '--file', required=True, help='File or directory to upload.')
 
 # parse arguments
 args = parser.parse_args()
@@ -19,13 +22,23 @@ persistentid = args.persistentid
 apitoken = args.apitoken
 file = args.file
 
-# prepare file
-file_content = 'content: %s' % datetime.now()
-files = {'file': (file, file_content)}
+def upload(file):
 
-# construct url with persistent id
-url_persistent_id = '%s/api/datasets/:persistentId/add?persistentId=%s&key=%s' % (dataverse, persistentid, apitoken)
+   # prepare file
+   file_content = 'content: %s' % datetime.now()
+   files = {'file': (file, file_content)}
 
-# fire off request
-r = requests.post(url_persistent_id, files=files)
-print r.json()
+   # construct url with persistent id
+   url_persistent_id = '%s/api/datasets/:persistentId/add?persistentId=%s&key=%s' % (dataverse, persistentid, apitoken)
+
+   # fire off request
+   r = requests.post(url_persistent_id, files=files)
+   print r.json()
+
+if os.path.isfile(file):
+   upload(file)
+
+if os.path.isdir(file):
+   filelist = [f for f in listdir(file) if isfile(join(file, f))]
+   for file in filelist:
+      upload(file)
