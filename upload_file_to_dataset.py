cd8d7d308d4cf1d5b8d3a421d8c4189adf0314c3
Don Sizemore <don.sizemore@gmail.com>
2019-08-14 10:42:29 -0400

    add upload_file_to_dataset script

---
+++ upload_file_to_dataset.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from datetime import datetime
+import argparse
+import json
+import requests
+
+# define arguments. persistentid, apitoken and file are required.
+parser = argparse.ArgumentParser(description='Upload a file to a given dataset.')
+parser.add_argument('-d', '--dataverse', default='https://dataverse.unc.edu', help='Dataverse URL. Defaults to https://dataverse.unc.edu')
+parser.add_argument('-p', '--persistentid', required=True, help='Persistent ID, in the format doi:authority/shoulder/identifier')
+parser.add_argument('-t', '--apitoken', required=True, help='API token. Required.')
+parser.add_argument('-f', '--file', required=True, help='File to upload.')
+
+# parse arguments
+args = parser.parse_args()
+dataverse = args.dataverse
+persistentid = args.persistentid
+apitoken = args.apitoken
+file = args.file
+
+# prepare file
+file_content = 'content: %s' % datetime.now()
+files = {'file': (file, file_content)}
+
+# dataset id
+url_persistent_id = '%s/api/datasets/:persistentId/add?persistentId=%s&key=%s' % (dataverse, persistentid, apitoken)
+
+r = requests.post(url_persistent_id, files=files)
+print r.json()
